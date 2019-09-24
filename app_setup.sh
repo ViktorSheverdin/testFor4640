0b70dd880205cd4b5ba46e302fec2c968f669696
root <root@localhost.localdomain>
2019-09-23 21:45:13 -0400

    almost there...

--- app_setup.sh
+++ app_setup.sh
@@ -1,3 +1,8 @@
+adduser admin
+echo P@ssw0rd | passwd admin --srdin
+usermod -aG wheel admin
+wget https://acit4640.y.vu/docs/module02/resources/acit_admin_id_rsa.pub -P ~admin/.ssh/authorized_keys
+
 sed -r -i 's/^(%wheel\s+ALL=\(ALL\)\s+)(ALL)$/\1NOPASSWD: ALL/' /etc/sudoers
 yum install epel-release vim git tcpdump curl net-tools bzip2 -y
 yum update -y
@@ -16,13 +21,15 @@ yum install nodejs npm -y
 yum install mongodb-server -y
 systemctl enable mongod && systemctl start mongod
 
-su - todo-app
-mkdir app
+
+mkdir /home/todo-app/app
+chown todo-app /home/todo-app/app
+cd /home/todo-app/app
 git clone https://github.com/timoguic/ACIT4640-todo-app.git .
 npm install
 
 #TODO replace the config file
-
+mv /home/admin/testing
 yum install nginx -y
 
 
